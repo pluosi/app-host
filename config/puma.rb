a84a8ee10e184edd52d885a680b59f817f4bfe60
ibcker <happymiyu@gmail.com>
2017-08-31 21:27:11 +0800

    fix: puma can not stop

--- config/puma.rb
+++ config/puma.rb
@@ -15,6 +15,8 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+pidfile "tmp/pids/puma.pid"
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
