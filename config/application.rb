9d634b8d52b93991dbfe53ac00be600e1262288f
ibcker <happymiyu@gmail.com>
2017-09-18 20:27:35 +0800

    修改时区

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module AppHost
     config.load_defaults 5.1
 
     config.i18n.default_locale = "zh-CN"
+
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :Beijing
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
