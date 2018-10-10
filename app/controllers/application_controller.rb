1b56e2c033fe534a92a9a6adf9d9b8fc4d3d0a02
ibcker <happymiyu@gmail.com>
2018-10-11 00:01:44 +0800

    retest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,29 +8,25 @@ class ApplicationController < ActionController::Base
 
   def udid_callback
 
-    profile_service_response = OpenSSL::PKCS7.new request.raw_post
+    profile_service_response = request.raw_post.to_s
 
-    # profile_service_response.verify profile_service_response.certificates, AppleDeviceX509Store, nil, OpenSSL::PKCS7::NOINTERN | OpenSSL::PKCS7::NOCHAIN
+    plistBegin = profile_service_response.index('<?xml version="1.0"')
+    plistEnd = profile_service_response.index('</plist>') + 8
+
+    plist = profile_service_response[plistBegin...plistEnd]
 
 
     Rails.logger.info "------profile_service_response start----"
     Rails.logger.info profile_service_response
     Rails.logger.info "------profile_service_response end----"
-    Rails.logger.info profile_service_response.data
+    Rails.logger.info "#{plistBegin} - #{plistEnd}"
+    Rails.logger.info "------"
+    Rails.logger.info "#{plist}"
     Rails.logger.info "------profile_service_response.data end----"
 
 
-    profile_service_attributes = CFPropertyList::List.new(:data => profile_service_response.data).value
+    profile_service_attributes = CFPropertyList::List.new(:data => plist).value
 
-    # Rails.logger.info "------udid_callback start----"
-    # Rails.logger.info request.raw_post
-    # Rails.logger.info "------2----"
-    # Rails.logger.info params.inspect
-    # Rails.logger.info "------udid_callback end----" 
-
-    File.open("/tmp/ota_helper_#{Time.now.to_i}.mobileconfig", 'wb') do |file|
-      file.write(request.raw_post)
-    end
 
     udid = profile_service_attributes.value['UDID'].value
     redirect_to "#{Settings.PROTOCOL}#{Settings.HOST}/udid/#{udid}"
