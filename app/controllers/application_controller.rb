adbb90daba23299b637fc2bbee8eaf2d2e6cbf06
ibcker <happymiyu@gmail.com>
2021-01-21 16:50:08 +0800

    fix verify_authenticity_token has not been defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
 
   include UserSign
